ebf1beb9add4df1a176f932e234617be5b5bf6be
Anthony Gress <the.techx.guy@gmail.com>
2024-12-07 12:27:46 -0500

    Update open-in-apollo.user.js

--- open-in-apollo.user.js
+++ open-in-apollo.user.js
@@ -1,10 +1,29 @@
 // ==UserScript==
 // @name         Open Apollo App
-// @version      1.0.2
+// @version      1.0.3
 // @author       AnthonyGress
 // @match        *://*.reddit.com/*
 // @downloadURL  https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
 // @updateURL    https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
 // @homepage     https://github.com/AnthonyGress/Open-In-Apollo/edit/main/open-in-apollo.user.js
 // ==/UserScript==
-window.location.href = `apollo://${window.location.hostname}${window.location.pathname}`
+function openInApollo() {
+	const regexRedditIDs = /^(?:https?:\/\/)?(?:(?:www|amp|m|i)\.)?(?:(?:reddit\.com))\/r\/(\w+)(?:\/comments\/(\w+)(?:\/\w+\/(\w+)(?:\/?.*?[?&]context=(\d+))?)?)?/i;
+  	const match = window.location.href.match(regexRedditIDs);
+	
+	if (window.location.pathname === '/' || window.location.pathname === '/?feed=home') {
+		window.location.href = `apollo://`
+		return;
+	}
+		
+	if (window.location.pathname.includes('/search')){
+		return;
+	}
+	
+	if (match) {
+		window.location.href = `apollo://${window.location.hostname}${window.location.pathname}`
+	}
+}
+
+openInApollo();
+
