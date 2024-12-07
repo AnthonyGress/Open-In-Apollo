2a3d687a4fcd48efb1b97c2ef5a821f067ce3afd
Anthony Gress <the.techx.guy@gmail.com>
2024-12-07 02:16:26 -0500

    Fix app not opening

--- open-in-apollo.user.js
+++ open-in-apollo.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Open Apollo App
-// @version      1.0.1
+// @version      1.0.2
 // @author       AnthonyGress
 // @match        *://*.reddit.com/*
 // @downloadURL  https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
 // @updateURL    https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
-// @homepage     https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/
+// @homepage     https://github.com/AnthonyGress/Open-In-Apollo/edit/main/open-in-apollo.user.js
 // ==/UserScript==
-if (window.location.pathname === '/redirect') return;
 window.location.href = `apollo://${window.location.hostname}${window.location.pathname}`
